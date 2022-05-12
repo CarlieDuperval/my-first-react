af0e1a901fce46017e4d7fe455234b65d7055772
Carlie Duperval <carlierichell@gmail.com>
2022-05-12 16:51:26 -0400

    Added react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+// this is a react components that return and jsx
 function App() {
+  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState("*")
+  const [isLoggedIn , setIsLoggedIn] = useState(false)
+  const loginLogout = () => setIsLoggedIn(!isLoggedIn)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>You click the button {count} times.</h1>
+        <button onClick={()=>setCount(count+ 1)}>+</button>
+        <button onClick={()=>setCount(0)}>Reset</button>
+        <button onClick={()=>setCount(count ? count- 1: 0)}>-</button>
+        <button onClick={()=>setCount(Math.max(count -1, 0))}>-</button>
+
+
+        <h2>Welcome {isLoggedIn? 'Logout': 'Login'}</h2>
+        {/* <h2> {isLoggedIn? 'Welcome Carlie': 'Bye'}</h2> */}
+        <button onClick={loginLogout}>{isLoggedIn? 'Logout': 'Login'}</button>
+        {/* <button onClick={()=>loginLogout()}>Login</button> */}
+
       </header>
+      <section>
+        <h2 onClick={()=>setFirstName(firstName + '*')}> Hello {firstName}.</h2>
+      </section>
     </div>
   );
 }
